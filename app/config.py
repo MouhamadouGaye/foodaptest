6f941e191c7478c54f50ab44ba72b21e66efb977
MouhamadouGaye <99024541+MouhamadouGaye@users.noreply.github.com>
2024-11-22 02:11:38 +0100

    Update config.py

--- app/config.py
+++ app/config.py
@@ -5,6 +5,8 @@ class Settings(BaseSettings):
     ALGORITHM: str ="HS256" # This sets the default algorithm
     DATABASE_URL:str
     MY_GMAIL_SECRET_KEY_CONTACT: str  # Add this field if you need it
+    POSTGRES_HOST: str = "127.0.0.1"  # Default value if not provided
+    POSTGRES_PORT: int = 5433  # Set to your custom PostgreSQL port (5433)
 
     class Config:
         env_file = ".env"  # This allows you to load settings from a .env file
